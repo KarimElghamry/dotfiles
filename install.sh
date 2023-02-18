385bd87ee27c472a99ee40ae4bace451941d8890
karim elghamry <karimelghamry@gmail.com>
2023-02-19 00:13:59 +0200

    add: install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+# install stow
+sudo apt-get update && sudo apt-get install stow
+
+# loop over folders
+for folder in */ ; do
+	# remove previous stows
+	stow -t $HOME -D $folder
+	
+	# mount new simlinks
+	stow -v -t $HOME $folder
+done
+
+# reload shell
+exec $SHELL -l
