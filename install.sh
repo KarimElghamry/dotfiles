562c4d0cb5535d5978cc5caa76cfb90133236faf
Karim Elghamry <karimelghamry@gmail.com>
2023-02-26 10:59:49 +0200

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 # install vim and related plugins
 sudo apt-get install vim -y
-git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki
-vim -c 'helptags ~/.vim/pack/plugins/start/vimwiki/doc' -c quit
 mkdir -p ~/.vim/colors/
 wget https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim -P ~/.vim/colors/
+git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki
+vim -c 'helptags ~/.vim/pack/plugins/start/vimwiki/doc' -c quit
 
 # install zsh and set as default shell
 sudo apt-get install zsh -y
