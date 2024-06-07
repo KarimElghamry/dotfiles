002b9b28e2f8de58da21089632a7476171447d76
Karim Elghamry <karimelghamry@gmail.com>
2024-06-07 17:40:41 +0100

    fix packer formatting
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

--- config/.config/nvim/lua/karim/packer.lua
+++ config/.config/nvim/lua/karim/packer.lua
@@ -1,47 +1,47 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  -- colorscheme
-  use {
-	  "folke/tokyonight.nvim",
-	  lazy = false,
-	  priority = 1000,
-	  opts = {},
-	  config = function()
-		  vim.cmd('colorscheme tokyonight-night')
-	  end
-  }
+    -- colorscheme
+    use {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd('colorscheme tokyonight-night')
+        end
+    }
 
-  -- treesitter
-  use {
-	  "nvim-treesitter/nvim-treesitter", {run = ':TSUpdate'}
-  }
+    -- treesitter
+    use {
+        "nvim-treesitter/nvim-treesitter", { run = ':TSUpdate' }
+    }
 
-  -- vim-fugitive
-  use {
-	  "tpope/vim-fugitive"
-  }
+    -- vim-fugitive
+    use {
+        "tpope/vim-fugitive"
+    }
 
-  -- lsp
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    -- lsp
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-		  {'neovim/nvim-lspconfig'},
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
-  }
+            { 'neovim/nvim-lspconfig' },
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
 end)
