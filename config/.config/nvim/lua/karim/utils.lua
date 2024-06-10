9130ccaf3185b26c88e5b24a800ec3f01fa67380
Karim Elghamry <karimelghamry@gmail.com>
2024-06-10 23:12:39 +0100

    fix toggleTerminal bug
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

--- config/.config/nvim/lua/karim/utils.lua
+++ config/.config/nvim/lua/karim/utils.lua
@@ -1,14 +1,19 @@
 local M = {}
 
-local termBuf = nil
+local termBuf = -1
+
+-- helper function to create a new terminal and save it in a buffer
+local function createTerminal()
+    vim.cmd.te()
+    termBuf = vim.api.nvim_get_current_buf()
+end
 
 -- opens a terminal in a new buffer,
--- or re-attaches the terminal buffer if already created
+-- or re-attaches the terminal buffer if already created.
 function M.toggleTerminal()
-    -- create a new terminal buffer if not created
-    if termBuf == nil then
-        vim.cmd.te()
-        termBuf = vim.api.nvim_get_current_buf()
+    -- create terminal buffer if not there
+    if not vim.api.nvim_buf_is_valid(termBuf) then
+        createTerminal()
         return
     end
 
