43ba5a15703bc0736e3030a33fc6e79af872ded6
karim_elghamry <karim_elghamry@dell.com>
2024-06-12 11:13:49 +0100

    add buffer finder to telescope

--- config/.config/nvim/after/plugin/colors.lua
+++ config/.config/nvim/after/plugin/colors.lua
@@ -1,7 +1,3 @@
--- force colorscheme
-local color = color or "tokyonight-night"
-vim.cmd.colorscheme(color)
-
 -- transparent background
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
