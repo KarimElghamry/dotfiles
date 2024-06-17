b67fa769796a6aa1f9c8e0987c08cba991cf3df7
Karim Elghamry <karimelghamry@gmail.com>
2024-06-17 18:49:37 +0100

    refactor link.sh to separate target stows
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

--- link.sh
+++ link.sh
@@ -1,22 +1,35 @@
+#!/bin/bash
+
+# consts
+BIN_FOLDER=/usr/local/bin/
+
 # install stow
 sudo apt-get update && sudo apt-get install stow -y
 
-# define folder ignore list
-ignore=("scripts/")
+# define folder lists
+home_stows=("config/" "tmux/" "vim/" "zsh/")
+bin_stows=("scripts/")
 
-# loop over folders
-for folder in */ ; do
-    if [[ ${ignore[@]} =~ $folder ]]
-    then
-        continue
-    fi
-
+# loop over home folder 
+for folder in "${home_stows[@]}"; do
+    echo "stowing $folder to $HOME"
 	# adopt existing files
 	stow --adopt -t $HOME $folder
 	git restore .
 
 	# mount new symlinks
 	stow -v --restow -t $HOME $folder
+done
+
+# loop over bin folders
+for folder in "${bin_stows[@]}"; do
+    echo "stowing $folder to $BIN_FOLDER"
+	# adopt existing files
+	sudo stow --adopt -t $BIN_FOLDER $folder
+	git restore .
+
+	# mount new symlinks
+	sudo stow -v --restow -t $BIN_FOLDER $folder
 done
 
 # reload shell
