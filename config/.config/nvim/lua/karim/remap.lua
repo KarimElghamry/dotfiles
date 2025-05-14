0e24c43b8ef5d1cf7043488b9d5d5b538389b644
Karim Elghamry <karimelghamry@gmail.com>
2025-05-14 19:12:48 +0100

    add yank to clipboard remap
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

--- config/.config/nvim/lua/karim/remap.lua
+++ config/.config/nvim/lua/karim/remap.lua
@@ -2,4 +2,5 @@ local utils = require("karim.utils")
 
 -- basic remaps
 vim.keymap.set("n", "<leader>tt", utils.toggleTerminal)
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set("t", "<F2>", [[<C-\><C-n>]])
