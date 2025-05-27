6dd4da8ed94c6fec8ddcbc904d820b5065f6dd3b
karim_elghamry <karim_elghamry@dell.com>
2025-05-27 15:45:05 +0100

    add docs to nvim config
    Signed-off-by: karim_elghamry <karim_elghamry@dell.com>

--- config/.config/nvim/lua/karim/config.lua
+++ config/.config/nvim/lua/karim/config.lua
@@ -1,2 +1,11 @@
 -- enable virtual line diagnostics
 vim.diagnostic.config({ virtual_text = true })
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
--- config/.config/nvim/lua/karim/set.lua
+++ config/.config/nvim/lua/karim/set.lua
@@ -1,19 +1,25 @@
+-- enable relative line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- 4 space tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- auto indent when inserting new line
 vim.opt.smartindent = true
 
+-- disable search highlighing
+-- enable case sensitive search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- enable true colors
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
