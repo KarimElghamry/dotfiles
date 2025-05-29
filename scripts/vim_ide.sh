02f4e499f9f14f26478a8e2c530c407b49dd6f53
karim_elghamry <karim_elghamry@dell.com>
2025-05-29 14:22:49 +0100

    refactor vim_ide tmux script
    Signed-off-by: karim_elghamry <karim_elghamry@dell.com>

--- scripts/vim_ide.sh
+++ scripts/vim_ide.sh
@@ -1,8 +1,31 @@
 #!/bin/zsh
 
-SESSION_NAME="vim IDE"
+open_session() {
+    NAME=$1
+    tmux new -d -s $NAME -n vim 
 
-tmux new -d -s $SESSION_NAME -n vim 
-tmux send -t $SESSION_NAME "vim ." Enter 
-tmux new-window -n shell 
-tmux attach -t $SESSION_NAME:0.0
+    # check if duplicate sessions
+    if [ $? -ne 0 ]; then
+        echo "attach to session? y/n"
+        read input
+
+        if [ $input = 'y' ] || [ $input = 'Y' ]; then
+            tmux attach -t $NAME
+            exit 0
+        fi
+    fi
+
+    # otherwise, create a new tmux sessions
+    tmux send -t $NAME "vim ." Enter 
+    tmux new-window -n shell 
+    tmux attach -t $NAME:0.0
+}
+
+SESSION_NAME="IDE" # default session name
+
+# get session name from args, if provided
+if [ -n "$1" ]; then
+    SESSION_NAME="$1"
+fi
+
+open_session $SESSION_NAME
