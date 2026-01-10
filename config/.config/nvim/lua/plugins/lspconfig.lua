5110f9788f47ff52129c7b844d50b0a004fc4692
Karim Elghamry <karimelghamry@gmail.com>
2026-01-10 19:50:45 +0000

    fix lspconfig in nvim
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

--- config/.config/nvim/lua/plugins/lspconfig.lua
+++ config/.config/nvim/lua/plugins/lspconfig.lua
@@ -3,11 +3,6 @@ return {
 	opts = {
 		automatic_enable = true,
 		ensure_installed = { "lua_ls", "gopls" },
-		handlers = {
-			function(server_name)
-				require("lspconfig")[server_name].setup({})
-			end,
-		},
 	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
