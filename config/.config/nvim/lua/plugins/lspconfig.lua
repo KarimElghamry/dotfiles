2c99f936bdf3ee6245edeae7ce4c7175387692d1
karim_elghamry <karim_elghamry@dell.com>
2026-04-29 09:48:07 +0100

    Fix lsp config
    Signed-off-by: karim_elghamry <karim_elghamry@dell.com>

--- config/.config/nvim/lua/plugins/lspconfig.lua
+++ config/.config/nvim/lua/plugins/lspconfig.lua
@@ -2,18 +2,12 @@ return {
 	"mason-org/mason-lspconfig.nvim",
 	opts = {
 		automatic_enable = true,
-<<<<<<< Updated upstream
-		ensure_installed = { "lua_ls", "gopls" },
-=======
 		ensure_installed = { "lua_ls", "gopls", "basedpyright" },
 		handlers = {
 			function(server_name)
-				if server_name ~= "basedpyright" then
 					require("lspconfig")[server_name].setup({})
-				end
 			end,
 		},
->>>>>>> Stashed changes
 	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
